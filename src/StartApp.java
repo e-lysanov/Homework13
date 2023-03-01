9c837198d3ca75040ce5cbd5c63e66d53953a952
e-lysanov <e.lysanov@yandex.ru>
2023-03-01 23:09:22 +0300

    2nd commit: ДЗ выполнено

--- src/StartApp.java
+++ src/StartApp.java
@@ -1,18 +1,53 @@
 public class StartApp {
     public static void main(String[] args) {
 
-        Author jackLondon = new Author("Jack", "London");
-        Book whiteFang = new Book("White Fang", jackLondon,1906);
+        Author jackLondon = new Author("Джек", "Лондон");
+        Book whiteFang = new Book("Белый Клык", jackLondon,1906);
 
         System.out.println("Название книги: " + whiteFang.getBookTitle());
         System.out.println("Имя автора: " + jackLondon.getNameOfAuthor());
         System.out.println("Фамилия автора: " + jackLondon.getSurnameOfAuthor());
         System.out.println("Год публикации книги: " + whiteFang.getPublicationYear());
 
+//        System.out.println("Автор: " + whiteFang.getAuthor().toString());
+
+//      использовал toString
+        System.out.println("Автор " + jackLondon);
+        System.out.println("whiteFang = " + whiteFang);
+
+
         whiteFang.setPublicationYear(1907);
         System.out.println("Измененный год публикации книги: " + whiteFang.getPublicationYear());
 
-        Author michaelBulgakov = new Author("Michael", "Bulgakov");
-        Book masterAndMargarita = new Book("Master and Margarita", michaelBulgakov, 1967);
+        Author michaelBulgakov = new Author("Михаил", "Булгаков");
+        Book masterAndMargarita = new Book("Мастер и Маргарита", michaelBulgakov, 1967);
+
+        //      сравню имена авторов
+        if (jackLondon.equals(michaelBulgakov)) {
+            System.out.println("Авторы тезки");
+        } else {
+            System.out.println("Имена авторов отличаются");
+        }
+
+        //      сравню названия произведений
+        if (whiteFang.equals(masterAndMargarita)) {
+            System.out.println("Названия книг совпали");
+        } else {
+            System.out.println("У книг разные названия");
+        }
+
+//      сравню произведения
+        if (masterAndMargarita.hashCode() == whiteFang.hashCode()) {
+            System.out.println("Книги одинаковые");
+        } else {
+            System.out.println("Книги разные");
+        }
+
+//      сравню авторов
+        if (michaelBulgakov.hashCode() == jackLondon.hashCode()) {
+            System.out.println("У этих книг один автор");
+        } else {
+            System.out.println("Книги написаны разными людьми");
+        }
     }
 }
